d915a605fec03eee76b930c47a93e2d4d0f011f0
Seena Khoee <seenakhoee@gmail.com>
2018-04-21 13:45:09 -0700

    first commit

---
+++ dev/index.js
@@ -0,0 +1,28 @@
+import css from './main.scss'
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
+
+if (module.hot) {
+    module.hot.accept()
+}
